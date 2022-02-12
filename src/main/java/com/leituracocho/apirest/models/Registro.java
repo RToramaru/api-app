2a827a5dca9f42a58465404f835979c11ee90e6a
Rafael Almeida <rafael.almeida.soares2012@gmail.com>
2022-02-12 16:58:45 -0300

    alteracao para colunas string

--- src/main/java/com/leituracocho/apirest/models/Registro.java
+++ src/main/java/com/leituracocho/apirest/models/Registro.java
@@ -18,11 +18,11 @@ public class Registro implements Serializable {
     @Id
     String cocho;
     @Id
-    double quant_inical;
+    String quant_inical;
     @Id
-    double quant_final;
+    String quant_final;
     @Id
-    double porcentagem;
+    String porcentagem;
     @Id
     String data;
 
@@ -31,7 +31,7 @@ public class Registro implements Serializable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Registro registro = (Registro) o;
-        return Double.compare(registro.quant_inical, quant_inical) == 0 && Double.compare(registro.quant_final, quant_final) == 0 && Double.compare(registro.porcentagem, porcentagem) == 0 && data.equals(registro.data) && aluno.equals(registro.aluno) && cocho.equals(registro.cocho);
+        return aluno.equals(registro.aluno) && cocho.equals(registro.cocho) && quant_inical.equals(registro.quant_inical) && quant_final.equals(registro.quant_final) && porcentagem.equals(registro.porcentagem) && data.equals(registro.data);
     }
 
     @Override
@@ -39,52 +39,11 @@ public class Registro implements Serializable {
         return Objects.hash(aluno, cocho, quant_inical, quant_final, porcentagem, data);
     }
 
-
     public String getAluno() {
         return aluno;
     }
 
     public void setAluno(String aluno) {
         this.aluno = aluno;
-    }
-
-    public String getCocho() {
-        return cocho;
-    }
-
-    public void setCocho(String cocho) {
-        this.cocho = cocho;
-    }
-
-    public double getQuant_inical() {
-        return quant_inical;
-    }
-
-    public void setQuant_inical(double quant_inical) {
-        this.quant_inical = quant_inical;
-    }
-
-    public double getQuant_final() {
-        return quant_final;
-    }
-
-    public void setQuant_final(double quant_final) {
-        this.quant_final = quant_final;
-    }
-
-    public double getPorcentagem() {
-        return porcentagem;
-    }
-
-    public void setPorcentagem(double porcentagem) {
-        this.porcentagem = porcentagem;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
     }
 }
